590d40c072bc5c5d4db6e621c2f392b0615b7b0e
zZDuyZz <minhduytmd06@gmail.com>
2024-11-23 13:30:09 +0700

    ViewManagement
    Function View

--- Wordpad/MainWindow.xaml.cs
+++ Wordpad/MainWindow.xaml.cs
@@ -1,28 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WordPadApp;
 
-namespace Wordpad
+namespace WordPad
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewManagment viewManagment;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Khởi tạo ViewManagment
+            viewManagment = new ViewManagment(rulerCanvas, statusBar, statusBarItem, textBox, unitComboBox);
+        }
+
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            viewManagment.ZoomIn();
+        }
+
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            viewManagment.ZoomOut();
+        }
+
+        private void UnitComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            viewManagment.UpdateRuler();
+        }
+
+        private void ToggleRuler_Click(object sender, RoutedEventArgs e)
+        {
+            viewManagment.ShowRuler(rulerCanvas.Visibility != Visibility.Visible);
+        }
+
+        private void ToggleStatusBar_Click(object sender, RoutedEventArgs e)
+        {
+            viewManagment.ShowStatusBar(statusBar.Visibility != Visibility.Visible);
+        }
+
+        private void ZoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (viewManagment != null)
+            {
+                viewManagment.SetZoom((int)((Slider)sender).Value);
+            }
         }
     }
 }
